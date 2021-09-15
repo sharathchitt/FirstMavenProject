3ed17e1e2e0a85b04511c1d34bc24c41998c4923
Sharath N Chittaragi <cnsharath14@gmail.com>
2021-09-15 12:33:08 +0530

    Added a Ticket module in com.lti.model

---
+++ LTIAirlines/src/main/java/com/lti/model/Ticket.java
@@ -0,0 +1,28 @@
+package com.lti.model;
+
+public class Ticket {
+	
+	private double price;
+	private int ticketID;
+	
+	Ticket(){
+		
+	}
+	
+	
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	public int getTicketID() {
+		return ticketID;
+	}
+	public void setTicketID(int ticketID) {
+		this.ticketID = ticketID;
+	}
+	
+	
+
+}
