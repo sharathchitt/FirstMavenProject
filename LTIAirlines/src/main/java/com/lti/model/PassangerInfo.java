9ec224ddbffe478687ef8f964d129f5c36945044
Sharath N Chittaragi <cnsharath14@gmail.com>
2021-09-15 13:19:12 +0530

    Started with Passanger class

---
+++ LTIAirlines/src/main/java/com/lti/model/PassangerInfo.java
@@ -0,0 +1,24 @@
+package com.lti.model;
+
+public class PassangerInfo {
+	
+	String passName;
+	int passID;
+	
+	
+	public String getPassName() {
+		return passName;
+	}
+	public void setPassName(String passName) {
+		this.passName = passName;
+	}
+	public int getPassID() {
+		return passID;
+	}
+	public void setPassID(int passID) {
+		this.passID = passID;
+	}
+	
+	
+
+}
